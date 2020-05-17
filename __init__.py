779f20b3f2e3112de3d4e0ea1ac863e27167c831
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2020-05-17 11:58:07 -0400

    ???

--- __init__.py
+++ __init__.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    objects = [None, None, None]
-    objects[0] = listing(None, None, None, None)
-    objects[1] = listing(None, None, None, None)
-    objects[2] = listing(None, None, None, None)
-    objects[0].setImg('static/images/pic01.jpg')
-    objects[1].setImg('static/images/pic02.jpg')
-    objects[2].setImg('static/images/pic03.jpg')
-    return render_template('index.html', objects=objects)
+    objects = []
+    itemsinrow = 3
+    objects = getContent(objects)
+    items = len(objects)
+    return render_template('index.html', objects=objects, itemsinrow=itemsinrow, items=items)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
+
+# Populates product listings
+def getContent(objects):
+    Directory = os.getcwd() + "/"
+    for count, filename in enumerate(os.listdir(Directory)):
+        objects.append(listing(None, None, None, None))
+        objects[count].setImg('static/images/dump/' + filename)
+    objects[0].setName("Air Force 1s")
+    objects[0].setPrice("$100.00")
+    objects[0].setDiscount("(20% off MSRP)")
+    return objects
+
+# Product listing object
 class listing:
     def __init__(self, img, name, price, discount):
         self.img = img
@@ -23,6 +37,19 @@ class listing:
     def setImg(self, img):
         self.img = img
 
+    def setName(self, name):
+        self.name = name
+
+    def setPrice(self, price):
+        self.price = price
+
+    def setDiscount(self, discount):
+        self.discount = discount
 
 if __name__ == '__main__':
+    # changes working directory to dump so that the product listings can be pushed
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir("static")
+    os.chdir("images")
+    os.chdir("dump")
     app.run(debug=True)
