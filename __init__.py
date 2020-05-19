d2f645721473a34fa1d26889a009490614927d97
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2020-05-18 21:34:50 -0400

    added scraping

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from PIL import Image
+import requests
+import json
+from io import BytesIO
 import os
 
 app = Flask(__name__)
@@ -13,26 +17,47 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    return render_template('login.html')
 
 # Populates product listings
 def getContent(objects):
-    Directory = os.getcwd() + "/"
-    for count, filename in enumerate(os.listdir(Directory)):
-        objects.append(listing(None, None, None, None))
-        objects[count].setImg('static/images/dump/' + filename)
-    objects[0].setName("Air Force 1s")
-    objects[0].setPrice("$100.00")
-    objects[0].setDiscount("(20% off MSRP)")
+    
+    os.getcwd()
+    os.chdir("scraping")
+
+    with open(str(os.getcwd()) + '\\NordstromRack.json') as f:
+        data = json.load(f)
+
+    for count, item in enumerate(data):
+        objects.append(listing())
+        objects[count].setName(item["title"])
+        objects[count].setPrice(item["price"])
+        objects[count].setDiscount(item["discount"])
+        objects[count].setBrand(item["brand"])
+        objects[count].setOriginal(item["retail-price"])
+        objects[count].setLink(item["link"])
+
+        #images and links are not right in json
+        #objects[count].setImg(grabImage(item["image-link"]))
+
     return objects
+
+# grabs image from url
+def grabImage(link):
+    response = requests.get(link)
+    img = Image.open(BytesIO(response.content))
+    return img
 
 # Product listing object
 class listing:
-    def __init__(self, img, name, price, discount):
-        self.img = img
-        self.name = name
-        self.price = price
-        self.discount = discount
+    def __init__(self):
+        self.img = None
+        self.name = None
+        self.brand = None
+        self.price = None
+        self.original = None
+        self.discount = None
+        self.link = None
 
     def setImg(self, img):
         self.img = img
@@ -40,16 +65,22 @@ class listing:
     def setName(self, name):
         self.name = name
 
+    def setBrand(self, brand):
+        self.brand = brand
+
     def setPrice(self, price):
         self.price = price
+
+    def setOriginal(self, original):
+        self.original = original
 
     def setDiscount(self, discount):
         self.discount = discount
 
+    def setLink(self, link):
+        self.link = link
+
+
+
 if __name__ == '__main__':
-    # changes working directory to dump so that the product listings can be pushed
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    os.chdir("static")
-    os.chdir("images")
-    os.chdir("dump")
     app.run(debug=True)
