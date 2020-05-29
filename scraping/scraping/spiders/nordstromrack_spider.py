2d7f9c8df951de34e7df679450bc1b648bef5813
csz5024 <caseyzduniak@gmail.com>
2020-05-29 08:25:12 -0400

    scraping working

--- scraping/scraping/spiders/nordstromrack_spider.py
+++ scraping/scraping/spiders/nordstromrack_spider.py
@@ -1,41 +1,30 @@
+
+
 import scrapy
-from scrapy.http import HtmlResponse
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1200x600')
-
-driver = webdriver.Chrome(executable_path="C:\\valuables\\chromedriver.exe", chrome_options=options)
-
-# body = driver.page_source
-# return HtmlResponse(driver.current_url, body=body, encoding='utf-8', request=request)
-
-# driver = webdriver.Chrome('home/roger/chromedriver')
-driver.get("https://www.nordstromrack.com/shop/Men/Clothing")
 
 
 class NordstromRackSpider(scrapy.Spider):
     name = "NordstromRack"
-    start_urls = "https://www.nordstromrack.com/shop/Men/Clothing"
-    driver.get(start_urls)
+    start_urls = ["https://www.nordstromrack.com/shop/Men/Clothing"]
+
 
     def parse(self, response):
         numitems = 0
         for article in response.css('div.product-grid-item'):
             numitems = numitems + 1
-            yield {
+            yield{
                 'title': article.css('.product-grid-item__title ::text').get(),
                 'brand': article.css('.product-grid-item__brand ::text').get(),
                 'retail-price': article.css('.product-grid-item__retail-price del::text').get(),
                 'price': article.css('.product-grid-item__sale-price ::text').get(),
                 'discount': article.css('.product-grid-item__sale-price-discount ::text').get(),
                 'image-link': article.css('.product-grid-item__catalog-image img::attr(src)').get(),
-                'link': article.css('.product-grid-item a::attr(href)').get()
+                'link': 'https://nordstormrack.com' + article.css('.product-grid-item a::attr(href)').get()
             }
-        next_page = response.css('a.pagination-link::attr(href)').get()
+        next_page = response.css('a.pagination__link::attr(href)')[-1].get()
+        #print('here')
+        #print(next_page)
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
-        driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
         print(numitems)
