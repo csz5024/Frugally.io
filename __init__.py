ef914c8744476a14b9ebe5afda5d451c38becd91
csz5024 <caseyzduniak@gmail.com>
2020-06-17 16:38:51 -0400

    sql statements

--- __init__.py
+++ __init__.py
@@ -65,7 +65,8 @@ def before_request():
 @app.route('/', methods=['GET'])
 def index():
 
-    mySQLscratch()
+    #mySQLscratch()
+    populateTables()
     nordstrom = getNordstromContent()
     nike = getNikeContent()
     fullList = nordstrom + nike
@@ -475,6 +476,29 @@ def getPrice(products, filters, highlow):
             i.setPrice("$"+str(i.price))
 
     return objects
+
+def populateTables():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    #cursor.execute('CREATE TABLE IF NOT EXISTS Nike(NAME CHAR(100), SEX CHAR(1), PRICE CHAR(10))')
+
+    with open('/var/www/Frugally/Frugally/nordstromracksales/NordstromRackMen.json') as f:
+        data = json.load(f)
+
+    for count, item in enumerate(data):
+        if(item['discount'] != None):
+            disc = item["discount"].split()
+            disc = disc[0]
+        else:
+            disc = "-0%"
+        sql = 'INSERT INTO NordstromRackMen(vendor, gender, title, brand, retailprice, price, discount, imagelink, link) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);'
+        val =  (str(item['vendor']), str(item['gender']), str(item['title']), str(item['brand']), str(item['retail-price']), str(item['price']), str(disc), str(item['image-link']), str(item['link']))
+
+        cursor.execute(sql, val)
+
+    conn.commit()
+    conn.close()
+    return 0
 
 
 # Populates product listings
