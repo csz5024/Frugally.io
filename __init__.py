ed305835d66d14f3a4e9dcdb9d1e93fec0cceb97
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2020-06-25 14:24:32 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,13 +19,6 @@ import sys
 sys.path.append("..")
 from Frugally import DBqueries
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="frugally",
-    password="Shoelas",
-    database="Frugally"
-)
-
 app = Flask(__name__)
 
 
@@ -59,9 +52,9 @@ def InternalError(e):
 def index():
 
     #nordstrom = getNordstromContent()
-    nordstrom = getSQLNordstrom()
+    nordstrom = DBqueries.getSQLNordstrom()
     #nike = getNikeContent()
-    nike = getSQLNike()
+    nike = DBqueries.getSQLNike()
     fullList = nordstrom + nike
     items = len(fullList)
     itemsinrow = 3
@@ -470,71 +463,6 @@ def getPrice(products, filters, highlow):
 
     return objects
 '''
-
-# The goal of this function is to return a set of products
-# whose attributes match that of the filters
-# and are sorted in order of highest discount to lowest
-def getSQLdiscount(filters):
-
-    # Parse out the filters
-    if(filters!=None):
-        gender = str(filters[0][1]).lower()
-        filtervendor = filters[1][1]
-        filterbrands = filters[2][1]
-    else:
-        gender = "all"
-        filtervendor = "all"
-        filterbrands = "all"
-
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM NordstromRackMen WHERE', gender)
-
-    item = cursor.fetchall()
-
-
-# The goal of this function is to return a set of products
-# whose attributes match that of the filters
-# and are sorted in order of price, depending on the boolean value of highlow
-def getSQLprice(filters, highlow):
-    pass
-
-
-#This function simply fetches all nordstromrack content
-def getSQLNordstrom():
-
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM NordstromRackMen')
-
-    item = cursor.fetchall()
-
-    cursor.execute('SELECT * FROM NordstromRackWomen')
-
-    item2 = cursor.fetchall()
-    item = item + item2
-    #for i in item:
-    #    print(i)
-
-    cursor.close()
-    return item
-
-
-#This funciton simply fetches all nike content
-def getSQLNike():
-
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM NikeMen')
-
-    item = cursor.fetchall()
-
-    cursor.execute('SELECT * FROM NikeWomen')
-
-    item2 = cursor.fetchall()
-    item = item + item2
-    #for i in item:
-    #    print(i)
-
-    cursor.close()
-    return item
 
 '''
 # Populates product listings
