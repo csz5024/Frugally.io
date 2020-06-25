f94ed3dad7d49e858116add75beb2e14b81a00f0
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2020-06-25 14:22:48 -0400

    Update DBqueries.py

--- DBqueries.py
+++ DBqueries.py
@@ -9,8 +9,38 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
+# The goal of this function is to return a set of products
+# whose attributes match that of the filters
+# and are sorted in order of highest discount to lowest
+def getSQLdiscount(filters):
+
+    # Parse out the filters
+    if(filters!=None):
+        gender = str(filters[0][1]).lower()
+        filtervendor = filters[1][1]
+        filterbrands = filters[2][1]
+    else:
+        gender = "all"
+        filtervendor = "all"
+        filterbrands = "all"
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM NordstromRackMen WHERE', gender)
+
+    item = cursor.fetchall()
+
+
+# The goal of this function is to return a set of products
+# whose attributes match that of the filters
+# and are sorted in order of price, depending on the boolean value of highlow
+def getSQLprice(filters, highlow):
+    pass
+
+
+#This function simply fetches all nordstromrack content
 def getSQLNordstrom():
 
+    cursor = conn.cursor()
     cursor.execute('SELECT * FROM NordstromRackMen')
 
     item = cursor.fetchall()
@@ -19,15 +49,17 @@ def getSQLNordstrom():
 
     item2 = cursor.fetchall()
     item = item + item2
-    for i in item:
-        print(i)
+    #for i in item:
+    #    print(i)
 
     cursor.close()
-    conn.close()
     return item
 
+
+#This funciton simply fetches all nike content
 def getSQLNike():
 
+    cursor = conn.cursor()
     cursor.execute('SELECT * FROM NikeMen')
 
     item = cursor.fetchall()
@@ -40,7 +72,5 @@ def getSQLNike():
     #    print(i)
 
     cursor.close()
-    conn.close()
     return item
-
 
