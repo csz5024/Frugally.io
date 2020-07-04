a5a4ac3375216104ac766846cdc808a48649885c
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2020-07-04 12:32:55 -0400

    Update nike_spider_men.py

--- nordstromracksales/nordstromracksales/spiders/nike_spider_men.py
+++ nordstromracksales/nordstromracksales/spiders/nike_spider_men.py
@@ -1,5 +1,4 @@
 
-
 import scrapy
 from scrapy.selector import Selector
 from selenium import webdriver
@@ -8,24 +7,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
-
-
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1200x900')
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-
-driver = webdriver.Chrome(executable_path="/var/www/Frugally/Frugally/chromedriver", chrome_options=options)
+import time
+import mysql.connector
+import json
+import sys
 
 
 class NikeMenSpider(scrapy.Spider):
     name = "NikeMen"
     start_urls = ["https://www.nike.com/w/mens-sale-3yaepznik1"]
 
-
     def parse(self, response):
+
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="frugally",
+            password="Shoelas",
+            database="Frugally"
+        )
+        cursor = conn.cursor()
 
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
@@ -43,16 +43,7 @@ class NikeMenSpider(scrapy.Spider):
 
         while True:
 
-            #url = "https://www.nike.com/w/mens-sale-3yaepznik1"
-            #self.driver.get(url )
-            numitems = 0
-
-            WebDriverWait(self.driver, 2)
-
-
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-            WebDriverWait(self.driver, 2)
 
             scrapy_selector = Selector(text=self.driver.page_source)
 
@@ -63,23 +54,71 @@ class NikeMenSpider(scrapy.Spider):
             data = self.driver.find_element_by_class_name('product-grid')
             imlist = data.find_elements_by_tag_name('img')
 
-            for i in range(0, len(response.css('div.product-card__body'))):
-                article = scraplist[i]
-                image = imlist[i].get_attribute('src')
-                yield {
-                    'vendor': 'Nike',
-                    'gender': 'Men',
-                    'title': article.css('div.product-card__title ::text').get(),
-                    'brand': 'Nike',
-                    'retail-price': article.css('div.css-31z3ik ::text').get(),
-                    'price': article.css('div.css-s56yt7 ::text').get(),
-                    'discount': None,
-                    'image-link': image,  # article.css('.product-grid-item__catalog-image img::attr(src)').get(),
-                    'link': article.css('.product-card__body a::attr(href)').get()
-                }
+            time.sleep(4)
 
             new_height = self.driver.execute_script("return document.body.scrollHeight")
+
             if new_height == last_height:
                 break
             last_height = new_height
-            print(numitems)
+
+        for i in range(0, len(scraplist)):
+            article = scraplist[i]
+            image = imlist[i].get_attribute('src')
+
+            vendor = 'Nike'
+            gender = 'Men'
+            title = article.css('div.product-card__title ::text').get()
+            brand = 'Nike'
+            retailprice = article.css('div.css-31z3ik ::text').get()
+            price = article.css('div.css-s56yt7 ::text').get()
+            discount = None
+            imagelink = image
+            link = article.css('.product-card__body a::attr(href)').get()
+
+            #This file is a script with no dependencies. It relays the content found in the json files to the database
+            #Now supports multiple processes
+
+
+            print('Adding NikeMens Content to Database, please wait...')
+
+            if((retailprice!=None) and (price!=None)):
+              if(len(retailprice)>4):
+                  retail = float(retailprice.strip("$").replace(',',''))
+              else:
+                  retail = float(retailprice.strip("$"))
+              if(len(price)>4):
+                  price = float(price.strip("$").replace(',',''))
+              else:
+                  price = float(price.strip("$"))
+              discount = round((1-(price/retail))*100)
+            else:
+              discount = 0
+            title = str(title.strip("Nike "))
+            sql = 'INSERT INTO NikeMenTemp(vendor, gender, title, brand, retailprice, price, discount, imagelink, link) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);'
+            link = link.strip("https://")
+            val =  (str(vendor), str(gender), title, str(brand), retail, price, discount, str(imagelink), link)
+
+            #print("NikeMen item number "+str(count))
+            cursor.execute(sql, val)
+            conn.commit()
+
+        # Removes Duplicate Rows
+        cursor.execute("CREATE TABLE tempNM SELECT DISTINCT * FROM NikeMenTemp;")
+        cursor.execute("ALTER TABLE NikeMenTemp RENAME junk;")
+        cursor.execute("ALTER TABLE tempNM RENAME NikeMenTemp;")
+        cursor.execute("DROP TABLE junk;")
+
+        # Drops old table
+        cursor.execute("DROP TABLE IF EXISTS NikeMen;")
+
+        # Swaps in new table
+        cursor.execute('ALTER TABLE NikeMenTemp RENAME TO NikeMen;')
+
+        # Replaces old temp tables
+        cursor.execute('CREATE TABLE NikeMenTemp LIKE NikeMen;')
+        conn.commit()
+        print("NikeMens... Done!")
+
+        cursor.close()
+        conn.close()
