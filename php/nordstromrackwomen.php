cfefe0249ab02ad3d94e88418e756999176c1221
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2020-07-23 11:43:31 -0400

    Update nordstromrackwomen.php

--- php/nordstromrackwomen.php
+++ php/nordstromrackwomen.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php
-        $output=exec("cd /var/www/Frugally/Frugally/nordstromracksales && sudo scrapy crawl NordstromRackWomen");
+        putenv('PYTHONPATH=/home/frugally/.local/lib/python3.6/site-packages');
+        $output=exec("cd /var/www/Frugally/Frugally/nordstromracksales && scrapy crawl NordstromRackWomen");
 
         echo "<pre>$output</pre>";
 ?>
